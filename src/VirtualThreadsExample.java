e41c4b0551f5efb6800a330514b2d45090c3a38a
Rafael Ernesto <rflernesto@gmail.com>
2024-04-28 13:51:55 -0400

    virtual thread example

--- src/VirtualThreadsExample.java
+++ src/VirtualThreadsExample.java
@@ -1,22 +1,25 @@
+import java.util.concurrent.Executors;
+
 public class VirtualThreadsExample {
-    public static void run() {
-        System.out.println("Hello from virtual thread: " + Thread.currentThread().getName());
+    public static void run() throws InterruptedException {
+        Thread.sleep(1000);
+        System.out.println("Hello from virtual thread: " + Thread.currentThread());
     }
 }
 
 
 class VirtualThreadsExampleApp {
     public static void main(String[] args) {
-        var builder = Thread.ofVirtual().name("thread-",0);
+        var executor = Executors.newVirtualThreadPerTaskExecutor();
 
-        var t1 = builder.start(VirtualThreadsExample::run);
-        var t2 = builder.start(VirtualThreadsExample::run);
-
-        try {
-            t1.join();
-            t2.join();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
+        for(int i = 0; i < 10000000; i++) {
+            executor.submit(() -> {
+                try {
+                    VirtualThreadsExample.run();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            });
         }
     }
 }
